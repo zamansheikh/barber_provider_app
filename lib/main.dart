700c6c20713e607f363a12fd399337b90fcfc838
Zaman Sheikh <zaman6545@gmail.com>
2025-01-13 19:29:59 +0600

    remove trash

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,9 @@
-import 'dart:convert';
-
 import 'package:barbar_provider/core/app_route/app_route.dart';
-import 'package:barbar_provider/core/utils/logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
 
 void main() async {
-  String apiEndPoint = 'http://192.168.10.99:5002/api/v1';
-  final response = await http.post(
-    Uri.parse('$apiEndPoint/auth/login'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      'email': 'kafikafi1922@gmail.com',
-      'password': 'kafikafi1922@gmail.com'
-    }),
-  );
-  AppLogger.e(response.body);
-
   runApp(const MyApp());
 }
 
